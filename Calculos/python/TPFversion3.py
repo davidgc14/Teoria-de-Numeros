0f41792db5dd6cf18827556371d4f4fd701f55fa
davidgc14 <curbelo.gc@gmail.com>
2022-03-17 19:37:51 +0100

    Terminado para caso generico

--- Calculos/python/TPFversion3.py
+++ Calculos/python/TPFversion3.py
@@ -1,28 +1,30 @@
-
+# Valores a introducir por el usuario:
 n = 34539388448652514879742656254743
 P = 1
 Q = -1
 
-k = 1
-U1 = [k, 1]
-U2 = [k+1, P]
-binary = bin(n)[2:]
+# Datos de las iteraciones:
+k = 0
+U1 = 0
+U2 = 1
+binary = bin(n+1)[2:]
 
 m = 0
+print("m", "k", "U1", "U2")
+print(m, k, U1, U2)
 
 for i in binary: 
     if i == '0':
         k *= 2
-        aux = [2 * U1[1] * U2[1] - P * U1[1]^2, U2[1]^2 - Q * U1[1]^2]
+        aux = [2 * U1 * U2 - P * (U1**2), (U2**2) - Q * (U1**2)]
     else:
         k = 2*k + 1
-        aux = [U2[1]^2 - Q * U1[1]^2, P * U2[1]^2 - 2 * Q * U1[1] * U2[1]]
+        aux = [(U2**2) - Q *(U1**2), P * (U2**2) - 2 * Q * U1 * U2]
 
-    U1 = [k, aux[0] % n]
-    U2 = [k+1, aux[1] % n]
+    U1 = aux[0] % n
+    U2 = aux[1] % n
 
     m += 1
-    print(m)
-    print(U1)
+    print(m, k, U1, U2)
 
 
